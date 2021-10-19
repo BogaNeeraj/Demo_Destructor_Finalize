c4ba5b63f4f52ef37fc43b5d9484c08ad0856047
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-19 10:52:31 +0530

    Add files via upload

---
+++ MyClass1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Distructor_Finalize
+{
+    public class MyClass : IDisposable
+    {
+        public void Display_msg()
+        {
+            Console.WriteLine("inside Display method()");
+        }
+
+       public  MyClass()
+        {
+            Console.WriteLine("Default constructor");
+
+        }
+       public  MyClass(string msg)
+        {
+            Console.WriteLine("parameterized constructor with method");
+        }
+
+
+       // public override string ToString() => GetType().Name;
+        //~MyClass() => Console.WriteLine($"the {ToString()} finalizer is executing.");
+
+        public void Dispose()
+        {
+            Console.WriteLine("freeing up resources using Dispose");
+
+            GC.SuppressFinalize(this);
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo_Distructor_Finalize
+{
+    class Program : MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo initializer");
+            MyClass obj1 = new MyClass();
+            obj1.Display_msg();
+            obj1.Dispose();
+
+            // MyClass obj2 = new MyClass("secrete messege");
+             //obj2.Display_msg();
+
+
+        }
+    }
+}
